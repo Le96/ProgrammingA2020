699007870b960ec4660aa889afe0a54f0a1016cb
Le96 <blitz1216@gmail.com>
2020-05-12 18:49:33 +0900

    Add lec01 ex-level sources.

--- lec01/BinarySearch.java
+++ lec01/BinarySearch.java
@@ -1,7 +1,24 @@
 package lec01;
 
+import java.util.BitSet;
+import java.util.Scanner;
+
+/**
+ * BinarySearch
+ */
 public class BinarySearch {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        BitSet S = new BitSet();
+        for (int n = 0; n < N; n++)
+            S.set(sc.nextInt());
+        int Q = sc.nextInt();
+        BitSet T = new BitSet();
+        for (int q = 0; q < Q; q++)
+            T.set(sc.nextInt());
+        sc.close();
+        T.and(S);
+        System.out.println(T.cardinality());
     }
 }
--- lec01/ExhaustiveSearch.java
+++ lec01/ExhaustiveSearch.java
@@ -1,6 +1,7 @@
 package lec01;
 
 import java.util.ArrayList;
+import java.util.BitSet;
 import java.util.List;
 import java.util.Scanner;
 
@@ -8,34 +9,32 @@ import java.util.Scanner;
  * ExhaustiveSearch
  */
 public class ExhaustiveSearch {
+    private static final int M_MAX = 2000;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        List<Integer> A = new ArrayList<>();
+        List<Integer> A = new ArrayList<>(N);
         for (int n = 0; n < N; n++)
             A.add(sc.nextInt());
-        int M = sc.nextInt();
-        for (int m = 0; m < M; m++) {
-            System.out.println(dfs(A, sc.nextInt()) ? "yes" : "no");
-        }
+        int Q = sc.nextInt();
+        List<Integer> M = new ArrayList<>(Q);
+        for (int q = 0; q < Q; q++)
+            M.add(sc.nextInt());
         sc.close();
+        BitSet bs = new BitSet();
+        bs.set(0); // initialize
+        bs = dynamic_programming(bs, A);
+        for (Integer m : M)
+            System.out.println(bs.get(m) ? "yes" : "no");
     }
 
-    private static boolean dfs(List<Integer> A, int q) {
-        int trial = q - A.get(0);
-        if (trial == 0)
-            return true;
-        else if (trial < 0)
-            return false;
-        else {
-            for (int start = 1; start < A.size(); start++) {
-                List<Integer> subA = A.subList(start, A.size());
-                if (dfs(subA, q))
-                    return true;
-                else if (dfs(subA, trial))
-                    return true;
-            }
+    private static BitSet dynamic_programming(BitSet bs, List<Integer> A) {
+        for (Integer a : A) {
+            BitSet newBS = (BitSet) bs.clone();
+            bs.stream().forEach(index -> newBS.set(index + a));
+            bs = newBS.get(0, M_MAX + 1);
         }
-        return false;
+        return bs;
     }
 }
