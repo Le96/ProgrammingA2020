714004c5dce534af50cdbfeb78cc60ab5d9db665
Le96 <blitz1216@gmail.com>
2020-05-14 10:17:19 +0900

    Update subroutine.

--- aoj_agg/batch.py
+++ aoj_agg/batch.py
@@ -2,23 +2,48 @@
 
 import csv
 
+from tqdm import tqdm
+
 import aggregator
+import const
 
 
 def main() -> None:
     data = parse_csv()
     accepted = aggregate(data)
-    print(accepted)
+    score = {}
+    for k, v in accepted.items():
+        score[k] = calc_score(v)
+    for k, v in score:
+        print(k, v)
 
 
-def parse_csv() -> dict:
-    csv_dict = csv.DictReader(open('./Aizu_Online_Judge_.csv', 'r'))
-    return csv_dict
+def calc_score(accepted_problems: list) -> int:
+    score_list = const.SCORE_LIST
+    score = 0
+    for score_dict in score_list:
+        for k, v in score_dict.items():
+            # only scoring problem for beginner.
+            if k != 1:
+                continue
+            if v in accepted_problems:
+                score += k
+    return score
 
 
-def aggregate(data) -> dict:
+def parse_csv() -> list:
+    csv_data = []
+    # Download it from Waseda Moodle.
+    with open('./Aizu_Online_Judge_.csv', 'r') as csv_fp:
+        csv_reader = csv.DictReader(csv_fp)
+        for line in csv_reader:
+            csv_data.append(line)
+    return csv_data
+
+
+def aggregate(data) -> list:
     result = {}
-    for line in data:
+    for line in tqdm(data):
         user_name = line['フルネーム']
         user_id = line['Q01_アカウント名']
         user_id = user_id.split('=')[-1] if '=' in user_id else user_id
